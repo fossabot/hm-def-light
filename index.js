e7e4f76cbe9e2cd7e320270c168390cf9fc2b1a2
Leonardo Botelho <sbotelho.leo@gmail.com>
2019-01-06 12:35:51 -0200

    Reword `index`

--- index.js
+++ index.js
@@ -45,43 +45,62 @@ export default ({create,
   // internally, only S.unchecked is used
   const {unchecked : U} = S
 
-  // B combinator; alias
-  const B = U.compose
+  const {
+    // Birds
+    I,
+    flip : C,
+    compose : B,
+
+    // 'idiomatic'
+    on,
+    map,
+    lift2,
+    lift3,
+    traverse,
+    equals,
+    concat,
+    groupBy,
+    Pair,
+    Either,
+    Right,
+    mapLeft
+
+  } = U
 
   // maybeToList :: Maybe a -> [a]
-  const maybeToList = U.lift2 (U.maybe) (U.empty) (U.of) (Array)
+  const maybeToList = lift2 (U.maybe) (U.empty) (U.of) (Array)
 
   // extractTypeLL :: Type -> Maybe Type
   const extractTypeLL = k => U.gets (_ => true) (["types", k, "type"])
 
   // adaptFn :: Type -> [Type]
-  const adaptFn = Fn => U.on (U.on (U.concat) (maybeToList))
-                             (U.flip (extractTypeLL) (Fn))
-                             ("$1") ("$2")
+  const adaptFn = Fn => on (on (concat) (maybeToList))
+                           (C (extractTypeLL) (Fn))
+                           ("$1") ("$2")
 
   // SupraHMLang :: StrMap Parser
   const SupraHMLang      = _SupraHMLang ({S, $ : SDef, Z, typeClasses, typeConstructors})
 
   // parseType :: Parser
-  const parseType        = B (U.map (adaptFn))
+  const parseType        = B (map (adaptFn))
                              (SupraHMLang.function)
 
   // groupConstraints :: [Pair Type TypeClass] -> [Pair Type [TypeClass]]
   const groupConstraints = 
-    B (U.map (U.lift2 (U.Pair)
-                      (xs => U.fst (xs[0]))
-                      (U.map (U.snd))))
-      (U.groupBy (U.on (U.equals) (B (U.prop ("name")) (U.fst))))
+    B (map (lift2 (Pair)
+                  (xs => U.fst (xs[0]))
+                  (map (U.snd))))
+      (groupBy (on (equals) (B (U.prop ("name")) (U.fst))))
 
   // constraintsFromPairs :: [Pair Type [TypeClass]] -> StrMap [TypeClass]
   const constraintsFromPairs = B (U.fromPairs)
-                                 (U.map (U.mapLeft (U.prop ("name"))))
+                                 (map (mapLeft (U.prop ("name"))))
 
   // parseConstraints :: [Rose TypeCarton] -> Either String (StrMap [TypeClass])
   const parseConstraints =
-    B (U.map (B (constraintsFromPairs) (groupConstraints)))
-      (U.traverse (U.Either)
-                  (SupraHMLang.constraint))
+    B (map (B (constraintsFromPairs) (groupConstraints)))
+      (traverse (Either)
+                (SupraHMLang.constraint))
 
   /* Exports */
   // :: String -> AnyFunction # -> AnyFunction
@@ -98,14 +117,14 @@ export default ({create,
                 const {name, constraints, type} = parseEarly (typeDeclaration)
 
                 // g :: Either String AnyFunction
-                const g = U.lift3 (_def)
-                                  (U.Right (name))
-                                  (parseConstraints (constraints))
-                                  (parseType        (type))
+                const g = lift3 (_def)
+                                (Right (name))
+                                (parseConstraints (constraints))
+                                (parseType        (type))
 
                 // :: AnyFunction
                 return U.either (err => {throw err})
-                                (U.I)
+                                (I)
                                 (g)
               })
 }
